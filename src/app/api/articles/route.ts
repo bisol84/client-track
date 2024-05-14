a34cc3ddd5e40e16c2c0032cdf60d9f9a6cbe01d
Sebastien Bisoglio <sbisoglio@gmail.com>
2024-05-14 17:53:32 +0200

    feat: creat article popup

---
+++ src/app/api/articles/route.ts
@@ -0,0 +1,40 @@
+import prisma from "@/lib/db";
+
+export const dynamic = "force-dynamic";
+
+// export async function POST(req: Request, res: Response) {
+//   try {
+//     const data = await req.formData();
+//     const result = await prisma.Clients.create({
+//       data: {
+//         firstname: data.get("firstname"),
+//         lastname: data.get("lastname"),
+//         address: data.get("address"),
+//         npa: parseInt(data.get("npa")),
+//         locality: data.get("locality"),
+//         phone: data.get("phone"),
+//         email: data.get("email"),
+//       },
+//     });
+//     return Response.json(result);
+//   } catch (error) {
+//     console.error("Erreur lors de l'insertion :", error);
+//     return Response.json(
+//       { error: "Une erreur est survenue lors de l'insertion." },
+//       { status: 500 },
+//     );
+//   }
+// }
+
+export async function GET(req: Request, res: Response) {
+  try {
+    const result = await prisma.Articles.findMany();
+    return Response.json(result);
+  } catch (error) {
+    console.error("Erreur lors de la sélection :", error);
+    return Response.json(
+      { error: "Une erreur est survenue lors de l'insertion." },
+      { status: 500 },
+    );
+  }
+}
